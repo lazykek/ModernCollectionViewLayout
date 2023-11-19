d3e0af6ad5561b3a98c69ac2b634017b47cf81f6
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2023-11-19 12:58:51 +0300

    Add diffable data source

--- ModernCollectionView/SecondViewController.swift
+++ ModernCollectionView/SecondViewController.swift
@@ -7,10 +7,18 @@
 
 import UIKit
 
+private enum Section {
+    case main
+}
+
+private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, String>
+private typealias DataSource = UICollectionViewDiffableDataSource<Section, String>
+
 final class SecondViewController: UIViewController {
 
     @IBOutlet private weak var collectionView: UICollectionView!
 
+    private var dataSource: DataSource?
     private lazy var items: [String] = {
         (0...6).map { String($0) }
     }()
@@ -18,44 +26,48 @@ final class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.dataSource = self.makeDataSource()
+        self.collectionView.dataSource = self.dataSource
         self.collectionView.register(
             UINib(nibName: "CustomCell", bundle: nil),
             forCellWithReuseIdentifier: "CustomCell"
         )
+
+        self.applySnapshot()
     }
 
     @IBAction private func remove() {
-        self.items.remove(at: 0)
-        self.collectionView.deleteItems(at: [IndexPath(row: 0, section: 0)])
+        self.items.removeFirst()
+        self.applySnapshot()
+    }
+
+    private func makeDataSource() -> DataSource {
+        UICollectionViewDiffableDataSource<Section, String>(
+            collectionView: self.collectionView
+        ) { collectionView, indexPath, number -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: "CustomCell",
+                for: indexPath
+            ) as? CustomCell
+            cell?.number = number
+            return cell
+        }
+    }
+
+    private func applySnapshot() {
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(self.items)
+        self.dataSource?.apply(snapshot, animatingDifferences: true)
     }
 }
 
 extension SecondViewController: UICollectionViewDelegate {
-
-}
-
-extension SecondViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        self.items.count
-    }
-
-    func collectionView(
-        _ collectionView: UICollectionView,
-        cellForItemAt indexPath: IndexPath
-    ) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: "CustomCell",
-            for: indexPath
-        ) as? CustomCell
-        cell?.number = items[indexPath.row]
-        return cell ?? UICollectionViewCell()
-    }
-
     func collectionView(
         _ collectionView: UICollectionView,
         didSelectItemAt indexPath: IndexPath
     ) {
         self.items.remove(at: indexPath.item)
-        collectionView.deleteItems(at: [indexPath])
+        self.applySnapshot()
     }
 }
